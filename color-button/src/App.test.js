afad42214b2e4393eff7f745844ef51a0ce78631
Can Polat <can.polatt@yahoo.com>
2023-06-26 22:10:15 +0300

    Click button to change color

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, logRoles, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial color, and updates when clicked", () => {
+  //const { container } = render(<App />);
+  //logRoles(container);
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the bckground color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Changed to red'
+  expect(colorButton).toHaveTextContent("Change to red");
 });
+
+test("button has correct inital text", () => {});
